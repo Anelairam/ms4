381d00f944dfff407196f566059e0202917027a3
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2021-12-26 22:04:43 +0000

    Setup dropdown for the booking information on the booking.html

--- static/js/base.js
+++ static/js/base.js
@@ -1,1 +1,8 @@
-alert("Javascript file is connected");
+var book_title = document.getElementsByClassName("title");
+
+
+for (var i=0; i<= book_title.length; i++){
+    book_title[i].addEventListener("click", function(){
+        this.nextElementSibling.classList.toggle("info_display");
+    });
+}
