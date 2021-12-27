8fe6245be511179769238b04ff459da5ebb830c8
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2021-12-27 19:01:17 +0000

    Create book now form

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from the_greenwich.forms import CustomSignupForm
+from allauth.account.forms import LoginForm
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    signupdisplay = CustomSignupForm
+    return render(request, 'home/index.html', context)
--- the_greenwich/settings.py
+++ the_greenwich/settings.py
@@ -62,6 +62,10 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+ACCOUNT_FORMS = {
+    'signup': 'the_greenwich.forms.CustomSignupForm',
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
