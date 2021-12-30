f31e619c39383f902deb1bd693f598fde73424fb
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2021-12-30 18:20:47 +0000

    Setup login and signup form in the main page

--- home/views.py
+++ home/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from .forms import MyCustomLoginForm, MySignupForm
+from the_greenwich.forms import CustomSignupForm
 
 
 def index(request):
     context = {
         'login_form': MyCustomLoginForm(), 
-        'signup_form': MySignupForm()
+        'signup_form': CustomSignupForm()
     }
     # signupdisplay = CustomSignupForm
     return render(request, 'home/index.html',context)
