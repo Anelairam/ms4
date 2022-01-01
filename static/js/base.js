4b0d8f5fb7c79be5850ee73883dd286f7b1b8bcd
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-01-01 22:14:21 +0000

    Add basic js on the tab

--- static/js/base.js
+++ static/js/base.js
@@ -1,8 +1,19 @@
 var book_title = document.getElementsByClassName("title");
+var tablink = document.getElementsByClassName("tablinks");
+var login = document.getElementById("login_container");
+var signup = document.getElementById("signup_container");
 
 
 for (var i=0; i<= book_title.length; i++){
     book_title[i].addEventListener("click", function(){
         this.nextElementSibling.classList.toggle("info_display");
+    });
+}
+
+for (var i=0; i<= tablink.length; i++){
+    tablink[i].addEventListener("click", function(){
+        if (i == 0){
+            login.style.display = "block";
+        }
     });
 }
