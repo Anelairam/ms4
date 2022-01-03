1c0f020be2c61b18b3790327f357cc9bc141e9b7
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-01-03 12:12:31 +0000

    Setup js function to toggle the signup and login tabs

--- static/js/base.js
+++ static/js/base.js
@@ -2,8 +2,9 @@ var book_title = document.getElementsByClassName("title");
 var login_tab = document.getElementById("login_tablink");
 var signup_tab = document.getElementById("signup_tablink");
 var tablinks = document.getElementsByClassName("tablinks");
+var tabcontent = document.getElementsByClassName("tabcontent");
 var login = document.getElementById("login_container");
-var signup = document.getElementById("signup_container");
+var signup = document.getElementById("singup_container");
 
 
 for (var i=0; i<= book_title.length; i++){
@@ -13,5 +14,16 @@ for (var i=0; i<= book_title.length; i++){
 }
 
 function activeTab(btn_name){
-    
+    for (var i=0; i<tablinks.length; i++){
+        tablinks[i].classList.remove("active");
+        tabcontent[i].style.display = "none";
+    }
+    if (btn_name == "login"){
+        login_tab.classList.add("active");
+        login.style.display = "block";
+    }
+    else if (btn_name == "signup"){
+        signup_tab.classList.add("active");
+        signup.style.display = "block";
+    }
 }
