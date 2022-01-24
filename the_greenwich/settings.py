21ce9c50bdee5da648c0bc7c600d941b9a2eadc6
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-01-24 21:15:01 +0000

    Bug fix

--- the_greenwich/settings.py
+++ the_greenwich/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get("HEROKU_HOSTNAME"), "localhost",]
+ALLOWED_HOSTS = [os.environ.get("HEROKU_HOSTNAME"), "localhost", "ci-ms4-app.herokuapp.com"]
 
 
 # Application definition
